94d2f1098fd39578c0aad0d080c120924f0bd8dd
justjcurtis <justjcurtis@gmail.com>
2023-08-22 22:22:18 +0100

    fix: exit if nothing on stdin

--- utils/stdin.go
+++ utils/stdin.go
@@ -9,10 +9,18 @@ import (
 )
 
 func GetStdIn() []string {
+	// TODO: check if nothing is piped in && exit
 	var lines []string
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+	in := bufio.NewScanner(os.Stdin)
+	stats, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
+	if stats.Size() == 0 {
+		os.Exit(0)
+	}
+	for in.Scan() {
+		lines = append(lines, in.Text())
 	}
 	return CleanUp(lines)
 }
