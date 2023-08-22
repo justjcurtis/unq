ff8eea8653544d02cd76b3b24c8596fab50d8125
justjcurtis <justjcurtis@gmail.com>
2023-08-23 00:09:40 +0100

    wip: flags

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strings"
 
@@ -14,19 +15,26 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "unq",
-	Short: "a cli tool for creating and analyzing unique sets of strings",
-	Long: `It can be used to create a unique set of strings from a file or stdin
+	Short: "unq is a cli tool for creating and analyzing unique sets of strings",
+	Long: `unq is a cli tool for creating and analyzing unique sets of strings
+
+It can be used to create a unique set of strings from a file or stdin
 or to compare two sets of strings to find the unique strings between them.
 unq can be used in many ways to analyze and manage sets of strings.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		showStats, _ := cmd.Flags().GetBool("stats")
+		showCount, _ := cmd.Flags().GetBool("count")
+		showPercent, _ := cmd.Flags().GetBool("percent")
 		lines := utils.GetStdIn()
+		length := len(lines)
 		delimiter, err := utils.GetDelimiter(lines)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		if len(lines) == 1 {
+		if length == 1 {
 			lines = strings.Split(lines[0], delimiter)
+			length = len(lines)
 		} else {
 			for i, line := range lines {
 				if strings.HasSuffix(line, delimiter) {
@@ -35,8 +43,26 @@ unq can be used in many ways to analyze and manage sets of strings.`,
 			}
 			delimiter += "\n"
 		}
-		unique, _ := utils.GetUnique(lines)
-		fmt.Println(strings.Join(unique, delimiter))
+		unique, m := utils.GetUnique(lines)
+		if showStats {
+			fmt.Println("Total Lines:", len(lines))
+			fmt.Println("Total Unique Lines:", len(unique))
+			fmt.Println("Total Duplicate Lines:", len(lines)-len(unique))
+		}
+		if showCount || showPercent {
+			for k, count := range m {
+				percentage := math.Round(float64(count)/float64(length)*10000) / 100
+				if showCount && showPercent {
+					fmt.Printf("%v: %v %v%%\n", k, count, percentage)
+				} else if showCount {
+					fmt.Printf("%v: %v\n", k, count)
+				} else {
+					fmt.Printf("%v: %v%%\n", k, percentage)
+				}
+			}
+		} else {
+			fmt.Println(strings.Join(unique, delimiter))
+		}
 	},
 }
 
@@ -58,5 +84,7 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("stats", "s", false, "output stats about the unique set")
+	rootCmd.Flags().BoolP("count", "c", false, "output count of entires in the unique set")
+	rootCmd.Flags().BoolP("percent", "p", false, "output percentage of entires in the unique set")
 }
